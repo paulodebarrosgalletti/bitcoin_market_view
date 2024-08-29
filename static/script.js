4ca6a1849caa9f802a34a90f800707a90193c7d9
paulobarrosg <paulodebarros315@gmail.com>
2024-08-29 16:45:26 -0300

    Adiction DogeCoin

--- static/script.js
+++ static/script.js
@@ -6,32 +6,6 @@ function fetchBitcoinPrice() {
 
       // Verificar se os dados estão corretos antes de atualizar o DOM
       if (data && data.current_price !== undefined) {
-        // Limpar o conteúdo anterior para evitar duplicação
-        priceElement.innerText = `
-            Name: ${data.name}
-            Symbol: ${data.symbol}
-            Price: $${data.current_price}
-            Market Cap: $${data.market_cap}
-            24h Change: ${data["24h_change"]}%
-        `;
-      } else {
-        console.error("Data format error:", data);
-      }
-    })
-    .catch((error) => console.error("Error fetching data:", error));
-}
-
-// Atualizar o preço a cada 5 segundos
-setInterval(fetchBitcoinPrice, 5000);
-function fetchBitcoinPrice() {
-  fetch("/crypto/bitcoin")
-    .then((response) => response.json())
-    .then((data) => {
-      const priceElement = document.getElementById("crypto-price");
-
-      // Verificar se os dados estão corretos antes de atualizar o DOM
-      if (data && data.current_price !== undefined) {
-        // Limpar o conteúdo anterior para evitar duplicação
         priceElement.innerText = `
             Name: ${data.name}
             Symbol: ${data.symbol}
@@ -48,3 +22,28 @@ function fetchBitcoinPrice() {
 
 // Atualizar o preço a cada 50 segundos
 setInterval(fetchBitcoinPrice, 50000);
+
+function fetchDogecoinPrice() {
+  fetch("/crypto/dogecoin")
+    .then((response) => response.json())
+    .then((data) => {
+      const priceElement = document.getElementById("crypto-price-dogecoin");
+
+      // Verificar se os dados estão corretos antes de atualizar o DOM
+      if (data && data.current_price !== undefined) {
+        priceElement.innerText = `
+            Name: ${data.name}
+            Symbol: ${data.symbol}
+            Price: $${data.current_price}
+            Market Cap: $${data.market_cap}
+            24h Change: ${data["24h_change"]}%
+        `;
+      } else {
+        console.error("Data format error:", data);
+      }
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}
+
+// Atualizar o preço a cada 50 segundos
+setInterval(fetchDogecoinPrice, 50000);
